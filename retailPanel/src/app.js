f5dbe3a639174105b6bb33c670d0d4f3a5dc52e9
Anees <anees@fancymonk.com>
2018-08-29 17:20:38 +0530

    Error

--- retailPanel/src/app.js
+++ retailPanel/src/app.js
@@ -1,14 +1,14 @@
-var rtApp = angular.module("rtApp", ['ngRoute']);
+var rtApp = angular.module('rtApp', ["ngRoute"]);
 
 rtApp.config(["$routerProvider", "$locationProvider",
   function($routerProvider, $locationProvider) {
     $routerProvider
     
-    .when("/", 
+    .when('/', 
     {
       templateUrl: "section/retailCompany/retail.tpl.html",
       controller: "RetailController"
     })
-    .when
+    
   }
 ]);
