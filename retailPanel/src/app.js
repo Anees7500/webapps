e19e4d61dea67f22c5be15afe2d3dbb77db29b8d
Anees <anees@fancymonk.com>
2018-08-29 14:02:07 +0530

    Added retail company section

---
+++ retailPanel/src/app.js
@@ -0,0 +1,10 @@
+var retail = angular.module('AngularApp', []);
+retail.config(['$routerProvider', '$locationProvider', function($routerProvider, $locationProvider){
+ $routerProvider
+ .when('/',{
+     templateUrl:'',
+     controller:''
+ })
+ when()
+
+}]);
