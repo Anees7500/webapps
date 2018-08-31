2056309b5f7a0cbd53e0eea19f16ccea87e91a99
Anees <anees@fancymonk.com>
2018-08-31 14:38:48 +0530

    Added menu

--- retailPanel/src/config.js
+++
@@ -1,2 +0,0 @@
-angular.module("rtApp.config", [])
-.constant("GetRetailMenuUrl", "http://www.fancymonk.com:9124/api/vendor/cash-n-carry/get-formatted-menu?vendorId=8&companyId=22&dayName=Wednesday");
