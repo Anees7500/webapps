06df6654d7d5aa009a2b6b82ee004fe621dc3e2a
Anees <anees@fancymonk.com>
2018-09-03 18:39:58 +0530

    footer work

---
+++ retailPanel/src/sections/retailCompany/retail.js
@@ -0,0 +1,10 @@
+// var app = angular.module('app', []);
+
+// app.controller('CommentController', function ($scope) {
+//        $scope.toggle = function () {
+//         $scope.node.decisionCollapse = !$scope.variable
+//         console.log($scope.variable);
+//     }
+// });
+
+
---
+++ retailPanel/src/sections/retailMenuItem/retailmenuItem.ctrl.js
@@ -0,0 +1,5 @@
+rtApp.controller('RetailmenuController', ['$scope', '$rootScope', '$route', '$http', 
+function ($scope,$rootScope,$route,$http) {
+    
+}
+])
