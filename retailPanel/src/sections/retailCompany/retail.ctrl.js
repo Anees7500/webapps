e84e5076afafa5fb478a5981dd4925b02cb35565
Anees <anees@fancymonk.com>
2018-09-14 17:27:42 +0530

    cart work fixed

--- retailPanel/src/sections/retailCompany/retail.ctrl.js
+++ retailPanel/src/sections/retailCompany/retail.ctrl.js
@@ -1,5 +1,5 @@
-rtApp.controller('RetailController', ['$scope', '$rootScope', '$route', '$http', 'GetRetailMenuUrl', '$routeParams', 'retailCompanyService','$mdDialog',
-    function ($scope, $rootScope, $route, $http, GetRetailMenuUrl , $routeParams, retailCompanyService, $mdDialog) {
+rtApp.controller('RetailController', ['$scope', '$rootScope', '$route', '$http', 'GetRetailMenuUrl', '$routeParams', 'retailCompanyService', '$mdDialog',
+    function ($scope, $rootScope, $route, $http, GetRetailMenuUrl, $routeParams, retailCompanyService, $mdDialog) {
 
 		// read companyName from path parameter
 		var compName = $routeParams.companyName;
@@ -7,106 +7,102 @@ rtApp.controller('RetailController', ['$scope', '$rootScope', '$route', '$http',
 		console.log("company Name : ", compName);
 
 		var companyDetail;
-    var dayName;
+		var dayName;
 
 		retailCompanyService.getCompanyDetail(compName).then(function (resp) {
 			companyDetail = resp.data.data.company;
-      dayName = resp.data.data.day;
+			dayName = resp.data.data.day;
 			console.log("dtls ", companyDetail);
-      console.log("dayName ", dayName);
+			console.log("dayName ", dayName);
 
-      retailCompanyService.getCompanyMenu(companyDetail.id,
-        companyDetail.assignedVendorId,
-        dayName).then(function (response) {
-          console.log(JSON.stringify(response.data.data.menus));
-    			$scope.menuNode = unflatten(response.data.data.menus);
-    			console.log("test", $scope.menuNode);
-  		});
+			retailCompanyService.getCompanyMenu(companyDetail.id,
+				companyDetail.assignedVendorId,
+				dayName).then(function (response) {
+				console.log(JSON.stringify(response.data.data.menus));
+				$scope.menuNode = unflatten(response.data.data.menus);
+				console.log("test", $scope.menuNode);
+			});
 
 		});
 
-    $scope.cartItems = {};
+		$scope.cartItems = {};
 
-    $scope.addToCart = function(itemObj){
-      console.log("item id  ",JSON.stringify(itemObj));
-      console.log("check ",$scope.cartItems[itemObj.id]);
-      if($scope.cartItems[itemObj.id])
-      {
-        console.log("item  present");
-        $scope.cartItems[itemObj.id].count = $scope.cartItems[itemObj.id].count + 1;
-      }
-      else{
-        console.log("item not present");
-        console.log("adding");
-        $scope.cartItems[itemObj.id] = {
-          obj:itemObj,
-          count:1
-        };
-      }
-      $scope.cartItems[itemObj.id].addedInCart=true;
-      console.log("cart items : ", JSON.stringify($scope.cartItems));
-    };
+		$scope.addToCart = function (itemObj) {
+			console.log("item id  ", JSON.stringify(itemObj));
+			console.log("check ", $scope.cartItems[itemObj.id]);
+			if ($scope.cartItems[itemObj.id]) {
+				console.log("item  present");
+				$scope.cartItems[itemObj.id].count = $scope.cartItems[itemObj.id].count + 1;
+			} else {
+				console.log("item not present");
+				console.log("adding");
+				$scope.cartItems[itemObj.id] = {
+					obj: itemObj,
+					count: 1
+				};
+			}
+			$scope.cartItems[itemObj.id].addedInCart = true;
+			console.log("cart items : ", JSON.stringify($scope.cartItems));
+		};
 
-    $scope.showCart = function(ev) {
-    $mdDialog.show({
-      controller: DialogController,
-      templateUrl: 'sections/retailCompany/cart.html',
-      parent: angular.element(document.body),
-      targetEvent: ev,
-      clickOutsideToClose:true,
-      fullscreen: $scope.customFullscreen,
-      locals: {
-        items: $scope.cartItems
-     }
-    })
-    .then(function(answer) {
-      $scope.status = 'You said the information was "' + answer + '".';
-    }, function() {
-      $scope.status = 'You cancelled the dialog.';
-    });
-  };
+		$scope.showCart = function (ev) {
+			$mdDialog.show({
+					controller: DialogController,
+					templateUrl: 'sections/retailCompany/cart.html',
+					parent: angular.element(document.body),
+					targetEvent: ev,
+					clickOutsideToClose: true,
+					fullscreen: $scope.customFullscreen,
+					locals: {
+						items: $scope.cartItems
+					}
+				})
+				.then(function (answer) {
+					$scope.status = 'You said the information was "' + answer + '".';
+				}, function () {
+					$scope.status = 'You cancelled the dialog.';
+				});
+		};
 
-  function DialogController($scope, $mdDialog, items) {
-    $scope.hide = function() {
-      $mdDialog.hide();
-    };
+		function DialogController($scope, $mdDialog, items) {
+			$scope.hide = function () {
+				$mdDialog.hide();
+			};
 
-    $scope.cancel = function() {
-      $mdDialog.cancel();
-    };
+			$scope.cancel = function () {
+				$mdDialog.cancel();
+			};
 
-    $scope.answer = function(answer) {
-      $mdDialog.hide(answer);
-    };
+			$scope.answer = function (answer) {
+				$mdDialog.hide(answer);
+			};
 
-    // console.log("cart items : ", JSON.stringify(items));
-    // console.log("cart items count : ",Object.keys(items).length);
+			// console.log("cart items : ", JSON.stringify(items));
+			// console.log("cart items count : ",Object.keys(items).length);
 
-    $scope.selectedCartItems =  items;
+			$scope.selectedCartItems = items;
 
-    $scope.addCount = function(val){
-      val.count = val.count + 1;
-    }
+			$scope.addCount = function (val) {
+				val.count = val.count + 1;
+			}
 
-    $scope.reduceCount = function(val){
-      if(val.count > 0)
-      {
-        val.count = val.count - 1;
-      }
-    }
+			$scope.reduceCount = function (val) {
+				if (val.count > 0) {
+					val.count = val.count - 1;
+				}
+			}
 
-    function getTotalPrice(obj)
-    {
-      var tp = 0;
-      // _.each(arr, function(ar){
-      //   tp = tp + (ar.price*ar.quantity);
-      // });
+			function getTotalPrice(obj) {
+				var tp = 0;
+				// _.each(arr, function(ar){
+				//   tp = tp + (ar.price*ar.quantity);
+				// });
 
-      return tp;
-    };
+				return tp;
+			};
 
-    $scope.totalPrice = getTotalPrice($scope.cartItems);
-  }
+			$scope.totalPrice = getTotalPrice($scope.cartItems);
+		}
 
 
 		// console.log("Inside retail controller: ");
@@ -127,7 +123,7 @@ rtApp.controller('RetailController', ['$scope', '$rootScope', '$route', '$http',
 			ngDialog.open({
 				template: 'sections/retailCompany/cart.html',
 				className: 'ngdialog-theme-default',
-        controller: '',
+				controller: '',
 			});
 		};
 	}]);
--- retailPanel/src/sections/retailCompany/retail.srv.js
+++ retailPanel/src/sections/retailCompany/retail.srv.js
@@ -1,17 +1,17 @@
-rtApp.factory('retailCompanyService', ['$http','$httpParamSerializerJQLike','$location','getCompanyDetailUrl','GetRetailMenuUrl',
-  function($http, $httpParamSerializerJQLike,$location,getCompanyDetailUrl,GetRetailMenuUrl) {
-    return {
-        getCompanyDetail: function(compName) {
-            console.log("in side service retail");
-            console.log("passsed value : ", compName);
-            return $http.get(getCompanyDetailUrl+compName);
-        },
-        getCompanyMenu: function(companyId, vendorId, dayName) {
-            console.log("in side menu service");
-            console.log("url : ", GetRetailMenuUrl);
-            console.log("passsed value : ", companyId, vendorId, dayName);
-            return $http.get(GetRetailMenuUrl+"vendorId="+vendorId+"&companyId="+ companyId +"&dayName="+dayName);
-        }
+rtApp.factory('retailCompanyService', ['$http', '$httpParamSerializerJQLike', '$location', 'getCompanyDetailUrl', 'GetRetailMenuUrl',
+  function ($http, $httpParamSerializerJQLike, $location, getCompanyDetailUrl, GetRetailMenuUrl) {
+		return {
+			getCompanyDetail: function (compName) {
+				console.log("in side service retail");
+				console.log("passsed value : ", compName);
+				return $http.get(getCompanyDetailUrl + compName);
+			},
+			getCompanyMenu: function (companyId, vendorId, dayName) {
+				console.log("in side menu service");
+				console.log("url : ", GetRetailMenuUrl);
+				console.log("passsed value : ", companyId, vendorId, dayName);
+				return $http.get(GetRetailMenuUrl + "vendorId=" + vendorId + "&companyId=" + companyId + "&dayName=" + dayName);
+			}
 
-    }
+		}
 }]);
