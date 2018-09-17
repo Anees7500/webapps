bac9753f5b831cb4f3de3f0f2458f094f939ab02
pallavi <pallavig033@gmail.com>
2018-09-17 14:55:58 +0530

    added login page to client

---
+++ clientPanel/src/sections/clientLogin/login.ctrl.js
@@ -0,0 +1,9 @@
+clientApp.controller('LoginController', ['$scope',
+  function($scope) {
+    console.log("hello i am isnide login contriller");
+    // $scope.login = function(client) {
+    //   // console.log("check",client);
+    //   clientLoginService.login(client, clientLoginServiceUrl);
+    // }
+  }
+]);
