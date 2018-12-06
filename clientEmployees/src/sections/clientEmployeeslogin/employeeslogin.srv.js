dde6e82bde210563f9ea09848a70ef780500fa7d
Mohd Anees <anees@fancymonk.com>
2018-12-06 12:52:19 +0530

    login page

---
+++ clientEmployees/src/sections/clientEmployeeslogin/employeeslogin.srv.js
@@ -0,0 +1,33 @@
+ceApp.factory('EmployeesLoginService', ['$http', '$httpParamSerializerJQLike',
+    function ($http, $httpParamSerializerJQLike) {
+        return {
+            login: function (clientCredential, LoginClientUrl) {
+                console.log("client credential message ", clientCredential);
+                $http({
+                    method: 'POST',
+                    url: LoginClientUrl,
+                    headers: {
+                        'Content-Type': 'application/x-www-form-urlencoded',
+                        'type': 'clientPanel',
+                        'scm': 'fancymonk',
+                        'Authorization': 'Basic WVhCd1FHWmhibU41Ylc5dWEyRmhZV0Z1OjE1OjJDOjJBOkZFOjUxOkQwOkM3OjNCOjQ2OjFGOkREOjk2Ojk0OkFGOjkyOkE2OjFGOjUyOjBEOkUz',
+                    },
+
+                    data: $httpParamSerializerJQLike(clientCredential)
+                }).then(function (response) {
+                    console.log("response logins ", JSON.stringify(response));
+                    if (response.data.status == 1) {
+                        // $rootScope.companyDetails = response.data.data.company;
+                        // $cookies.put("clientPanelCompanyId", response.data.data.company.id);
+                        // $location.path('/dashboard');
+                    } else {
+                        // Notification.error('Username/Mobile/Email or password is incorrect');
+                    }
+                })
+                    .catch(function (response) {
+                        // Notification.error('Server is down please try again latter');
+                    });
+            }
+        }
+    }
+]);
