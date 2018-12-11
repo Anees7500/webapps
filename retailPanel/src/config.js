c5f47df3a7cfa4c284892b50323ff14ef4c75529
aman <aman.telkar@gmail.com>
2018-12-11 23:06:54 +0530

    fixed unessesary files

---
+++ retailPanel/src/config.js
@@ -0,0 +1,4 @@
+angular.module("rtApp.config", [])
+.constant("getCompanyDetailUrl", "http://fancymonk.com:9124/api/retail/corporate-companies/get-company-detail?companyName=")
+.constant("GetRetailMenuUrl", "http://www.fancymonk.com:9124/api/vendor/cash-n-carry/get-formatted-menu?")
+.constant("adminLoginUrl", "");
