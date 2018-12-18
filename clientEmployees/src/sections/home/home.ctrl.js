d262ee946bd244cd71269f940669e92dcceded19
Mohd Anees <anees@fancymonk.com>
2018-12-18 11:57:15 +0530

    sidenav bar fixed in clientemployees

--- clientEmployees/src/sections/home/home.ctrl.js
+++ clientEmployees/src/sections/home/home.ctrl.js
@@ -1,27 +1,23 @@
 ceApp.controller('HomeController', ['$scope', '$rootScope', '$http', '$log', 'homeService',
-'Notification', 'getWeeklyMenuUrl', '$cookies',
-  function($scope, $rootScope, $http, $log, homeService, Notification, getWeeklyMenuUrl,$cookies) {
+  'Notification', 'getWeeklyMenuUrl', '$cookies',
+  function ($scope, $rootScope, $http, $log, homeService, Notification, getWeeklyMenuUrl, $cookies) {
     var self = $scope;
     self.type = "Lunch";
     self.employeeComingList = [];
 
-    $scope.enableBooleans = function(value){
+    $scope.enableBooleans = function (value) {
       $scope.homeBool = false;
       $scope.weekMenuBool = false;
       $scope[value] = true;
     }
     $scope.enableBooleans("homeBool");
 
-    $scope.ShowHide = function(val)
-    {
+    $scope.ShowHide = function (val) {
       val.visible = val.visible ? false : true;
     }
 
-
-
-
     $http.get("http://fancymonk.com:9124/api/client/get-time-of-day-and-tmr-date")
-      .then(function(response) {
+      .then(function (response) {
         $log.info(' get time of the day  ' + JSON.stringify(response));
         if (response.data.timeOfDay != null) {
           $log.info(' hello if condition ');
@@ -34,7 +30,7 @@ ceApp.controller('HomeController', ['$scope', '$rootScope', '$http', '$log', 'ho
           self.tomorrowDate = response.data.tomorrowDate;
           var empUrl = "http://fancymonk.com:9125/api/client/get-employee-default-data?employeeRowId=77&startDate=2018-12-24&endDate=2018-12-29";
           $http.get(empUrl)
-            .then(function(response) {
+            .then(function (response) {
               if (response.data.data != null) {
                 $log.info('response  dchjb' + JSON.stringify(response));
                 for (var m = 0; m < response.data.data.details.length; m++) {
@@ -59,135 +55,102 @@ ceApp.controller('HomeController', ['$scope', '$rootScope', '$http', '$log', 'ho
       });
 
 
-      var weeklyMenuUrl = getWeeklyMenuUrl + $cookies.get("cId");
+    var weeklyMenuUrl = getWeeklyMenuUrl + $cookies.get("cId");
 
 
-      // var menu = JSON.parse(data.data.menus.menu);
-      $scope.weekMenuSorted = {};
-      $scope.weekMenuSorted.BREAKFAST = {};
-      $scope.weekMenuSorted.LUNCH = {};
-      $scope.weekMenuSorted.DINNER = {};
-      $scope.weekMenuSorted.SNACKS = {};
-      //  weekMenuSorted.BREAKFAST = {};
-      // weekMenuSorted.BREAKFAST = {};
+    // var menu = JSON.parse(data.data.menus.menu);
+    $scope.weekMenuSorted = {};
+    $scope.weekMenuSorted.BREAKFAST = {};
+    $scope.weekMenuSorted.LUNCH = {};
+    $scope.weekMenuSorted.DINNER = {};
+    $scope.weekMenuSorted.SNACKS = {};
+    //  weekMenuSorted.BREAKFAST = {};
+    // weekMenuSorted.BREAKFAST = {};
 
-      $http.get(weeklyMenuUrl).then(function(response)
-      {
-        // console.log("company weeklymenu response ", response);
-        $scope.weeklymenuSorted = [];
-        var weeklymenuSorted = response.data.data.menus;
-        for(var i =0; i<weeklymenuSorted.length; i++)
-        {
-          var tem = weeklymenuSorted[i];
-          tem.menu = JSON.parse(tem.menu);
+    $http.get(weeklyMenuUrl).then(function (response) {
+      // console.log("company weeklymenu response ", response);
+      $scope.weeklymenuSorted = [];
+      var weeklymenuSorted = response.data.data.menus;
+      for (var i = 0; i < weeklymenuSorted.length; i++) {
+        var tem = weeklymenuSorted[i];
+        tem.menu = JSON.parse(tem.menu);
 
-          if(tem.menuType === "BREAKFAST")
-          {
-            makeDayWiseJson(tem, "BREAKFAST", $scope.weekMenuSorted)
-          }
-          if(tem.menuType === "LUNCH")
-          {
-            makeDayWiseJson(tem, "LUNCH", $scope.weekMenuSorted)
-          }
-          if(tem.menuType === "DINNER")
-          {
-            makeDayWiseJson(tem, "DINNER", $scope.weekMenuSorted)
-          }
-          if(tem.menuType === "SNACKS")
-          {
-            makeDayWiseJson(tem, "SNACKS", $scope.weekMenuSorted)
-          }
+        if (tem.menuType === "BREAKFAST") {
+          makeDayWiseJson(tem, "BREAKFAST", $scope.weekMenuSorted)
         }
-        console.log(" soretd new json ", JSON.stringify($scope.weekMenuSorted));
+        if (tem.menuType === "LUNCH") {
+          makeDayWiseJson(tem, "LUNCH", $scope.weekMenuSorted)
+        }
+        if (tem.menuType === "DINNER") {
+          makeDayWiseJson(tem, "DINNER", $scope.weekMenuSorted)
+        }
+        if (tem.menuType === "SNACKS") {
+          makeDayWiseJson(tem, "SNACKS", $scope.weekMenuSorted)
+        }
+      }
+      console.log(" soretd new json ", JSON.stringify($scope.weekMenuSorted));
 
-      });
+    });
 
-      function makeDayWiseJson(tem, menutype, obj)
-      {
-        if(obj[menutype].Monday != null)
-        {
+    function makeDayWiseJson(tem, menutype, obj) {
+      if (obj[menutype].Monday != null) {
 
-        }
-        else
-        {
-          obj[menutype].Monday = {};
-        }
-        if(obj[menutype].Tuesday != null)
-        {
+      } else {
+        obj[menutype].Monday = {};
+      }
+      if (obj[menutype].Tuesday != null) {
 
-        }
-        else
-        {
-          obj[menutype].Tuesday = {};
-        }
-        if(obj[menutype].Wednesday != null)
-        {
+      } else {
+        obj[menutype].Tuesday = {};
+      }
+      if (obj[menutype].Wednesday != null) {
 
-        }
-        else
-        {
-          obj[menutype].Wednesday = {};
-        }
-        if(obj[menutype].Thursday != null)
-        {
+      } else {
+        obj[menutype].Wednesday = {};
+      }
+      if (obj[menutype].Thursday != null) {
 
-        }
-        else
-        {
-          obj[menutype].Thursday = {};
-        }
-        if(obj[menutype].Friday != null)
-        {
+      } else {
+        obj[menutype].Thursday = {};
+      }
+      if (obj[menutype].Friday != null) {
 
-        }
-        else
-        {
-          obj[menutype].Friday = {};
-        }if(obj[menutype].Saturday != null)
-        {
+      } else {
+        obj[menutype].Friday = {};
+      }
+      if (obj[menutype].Saturday != null) {
 
-        }
-        else
-        {
-          obj[menutype].Saturday = {};
-        }if(obj[menutype].Sunday != null)
-        {
+      } else {
+        obj[menutype].Saturday = {};
+      }
+      if (obj[menutype].Sunday != null) {
 
-        }
-        else
-        {
-          obj[menutype].Sunday = {};
-        }
+      } else {
+        obj[menutype].Sunday = {};
+      }
 
-        if(tem.dayName == "Monday")
-        {
-          obj[menutype].Monday = tem;
-        }
-        if(tem.dayName == "Tuesday")
-        {
-          obj[menutype].Tuesday = tem;
-        }
-        if(tem.dayName == "Wednesday")
-        {
-          obj[menutype].Wednesday = tem;
-        }
-        if(tem.dayName == "Thursday")
-        {
-          obj[menutype].Thursday = tem;
-        }
-        if(tem.dayName == "Friday")
-        {
-          obj[menutype].Friday = tem;
-        }
-        if(tem.dayName == "Saturday")
-        {
-          obj[menutype].Saturday = tem;
-        }
-        if(tem.dayName == "Sunday")
-        {
-          obj[menutype].Sunday = tem;
-        }
+      if (tem.dayName == "Monday") {
+        obj[menutype].Monday = tem;
+      }
+      if (tem.dayName == "Tuesday") {
+        obj[menutype].Tuesday = tem;
+      }
+      if (tem.dayName == "Wednesday") {
+        obj[menutype].Wednesday = tem;
+      }
+      if (tem.dayName == "Thursday") {
+        obj[menutype].Thursday = tem;
+      }
+      if (tem.dayName == "Friday") {
+        obj[menutype].Friday = tem;
+      }
+      if (tem.dayName == "Saturday") {
+        obj[menutype].Saturday = tem;
+      }
+      if (tem.dayName == "Sunday") {
+        obj[menutype].Sunday = tem;
+      }
 
-      };
+    };
   }
 ]);
