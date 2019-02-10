205556aabb4eb67bc0081e9fb343394b4e5da756
pallavi <pallavig033@gmail.com>
2019-02-10 13:55:18 +0530

    home page

--- smartCafeteria/src/sections/smartCafe_dashboard/dashboard.ctrl.js
+++ smartCafeteria/src/sections/smartCafe_dashboard/dashboard.ctrl.js
@@ -13,7 +13,30 @@ empApp.controller('DashboardController', ['$scope',
       $scope.termsAndPolicyBool = false;
       $scope[value] = true;
     }
-
     $scope.boolFunction("homeBool");
+
+    $scope.venderList = [
+      { venderName: "Meghana Foods", area: "Indiranagar "},
+      { venderName: "Absolute Barbecues", area: "Koramangala "},
+      { venderName: "The Bier Library", area: "Marathahalli "   },
+      { venderName: "Meghana Foods", area: "Indiranagar " },
+      { venderName: "Absolute Barbecues", area: "Koramangala " },
+      { venderName: "The Bier Library", area: "Marathahalli " },
+      { venderName: "Meghana Foods", area: "Indiranagar " },
+      { venderName: "Absolute Barbecues", area: "Koramangala "},
+      { venderName: "The Bier Library", area: "Marathahalli " },
+      { venderName: "Big Pitcher", area: "Old Airport Road " }
+      // { vanderName: "Meghana Foods", area: "JP Nagar Bangalore" }
+    ];
+
+    $scope.menuList = [
+      { menuType: "Breakfast"},
+      { menuType: "Lunch"},
+      { menuType: "Snacks"},
+      { menuType: "Dinner"},
+      { menuType: "Dosa"},
+      { menuType: "Combos"},
+      { menuType: "Chaat"}
+    ];
   }
 ]);																
