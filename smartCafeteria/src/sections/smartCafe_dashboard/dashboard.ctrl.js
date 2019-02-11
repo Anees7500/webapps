6680b612109c8f7098df8f4597789359fac2fc25
pallavi <pallavig033@gmail.com>
2019-02-11 18:30:26 +0530

    Feed back in smartCafeteris

--- smartCafeteria/src/sections/smartCafe_dashboard/dashboard.ctrl.js
+++ smartCafeteria/src/sections/smartCafe_dashboard/dashboard.ctrl.js
@@ -1,13 +1,13 @@
 empApp.controller('DashboardController', ['$scope',
-  function($scope) {
+  function ($scope) {
 
     // ------------boolfunction ---------------
     $scope.boolFunction = function (value) {
       console.log("boolFunction", value);
-      $scope.homeBool = false;  
-      $scope.myOdersBool = false; 
-      $scope.notificationBool = false; 
-      $scope.walletBool = false; 
+      $scope.homeBool = false;
+      $scope.myOdersBool = false;
+      $scope.notificationBool = false;
+      $scope.walletBool = false;
       $scope.feedbackBool = false;
       $scope.settingsBool = false;
       $scope.termsAndPolicyBool = false;
@@ -16,39 +16,60 @@ empApp.controller('DashboardController', ['$scope',
     $scope.boolFunction("homeBool");
 
     $scope.venderList = [
-      { venderName: "Meghana Foods", area: "Indiranagar "},
-      { venderName: "Absolute Barbecues", area: "Koramangala "},
-      { venderName: "The Bier Library", area: "Marathahalli "   },
       { venderName: "Meghana Foods", area: "Indiranagar " },
       { venderName: "Absolute Barbecues", area: "Koramangala " },
       { venderName: "The Bier Library", area: "Marathahalli " },
       { venderName: "Meghana Foods", area: "Indiranagar " },
-      { venderName: "Absolute Barbecues", area: "Koramangala "},
+      { venderName: "Absolute Barbecues", area: "Koramangala " },
+      { venderName: "The Bier Library", area: "Marathahalli " },
+      { venderName: "Meghana Foods", area: "Indiranagar " },
+      { venderName: "Absolute Barbecues", area: "Koramangala " },
       { venderName: "The Bier Library", area: "Marathahalli " },
       { venderName: "Big Pitcher", area: "Old Airport Road " }
       // { vanderName: "Meghana Foods", area: "JP Nagar Bangalore" }
     ];
 
     $scope.menuList = [
-      { menuType: "Breakfast"},
-      { menuType: "Lunch"},
-      { menuType: "Snacks"},
-      { menuType: "Dinner"},
-      { menuType: "Dosa"},
-      { menuType: "Combos"},
-      { menuType: "Chaat"}
+      { menuType: "Breakfast" },
+      { menuType: "Lunch" },
+      { menuType: "Snacks" },
+      { menuType: "Dinner" },
+      { menuType: "Dosa" },
+      { menuType: "Combos" },
+      { menuType: "Chaat" }
     ];
 
 
 
-
-
     //Ratingfeedback
-    $scope.maxRating = 5;
-    $scope.ratedBy = 0;
-    $scope.rateBy = function (star) {
-        $scope.ratedBy = star;
+    var maxRating = 5;
+    $scope.stars = [].constructor(maxRating);
+    $scope.ratingParameters = [
+      {
+        name: "Presentation",
+        rating : 3
+      },
+      {
+        name: "Quality",
+        rating : 3
+      },
+      {
+        name: "Taste",
+        rating : 4    
+        
+      },
+      {
+        name: "Quantity",
+        rating : 4    
+      }
+    ];
+    
+    $scope.rateBy = function (j, star) {
+      console.log("hhhsh cdhbdsjhs : ", star);
+      console.log("hhhshs : ", JSON.stringify($scope.stars));
+      j.rating = star;
+    
     }
-   
+
   }
 ]);																
