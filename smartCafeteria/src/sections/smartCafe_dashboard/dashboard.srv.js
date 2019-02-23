c56402265419c5871d03dee8b6312a76869c028e
aman <aman.telkar@gmail.com>
2019-02-23 13:03:49 +0530

    monthly details

--- smartCafeteria/src/sections/smartCafe_dashboard/dashboard.srv.js
+++ smartCafeteria/src/sections/smartCafe_dashboard/dashboard.srv.js
@@ -1,7 +1,16 @@
 empApp.factory('DashboardService', ['$http', '$httpParamSerializerJQLike','$location',
     function ($http, $httpParamSerializerJQLike,$location) {
         return {
-            
+            saveBookings: function (data) {
+                return $http({
+                    method: 'POST',
+                    url: postSmartCafeteriaBookingUrl,
+                    headers: {
+                        'Content-Type': 'application/x-www-form-urlencoded',
+                    },
+                    data: $httpParamSerializerJQLike(data)
+                });
+            }
         }
     }
 ]);
