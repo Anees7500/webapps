cbdd112dd1f99eacaf93ccfb02a3645cf7c7c64d
Anees <anees@fancymonk.com>
2019-03-22 18:50:34 +0530

    card work and table

--- salesPanel/src/config.js
+++ salesPanel/src/config.js
@@ -3,4 +3,5 @@ angular.module("salesApp.config", [])
 .constant("getImageUrl", "http://fancymonk.com:9124/companyImages/400/")
 .constant("getAllCompanyUrl", "http://fancymonk.com:9125/api/admin/corporate-companies?type=all")
 .constant("getAllVendorListUrl", "http://fancymonk.com:9125/api/admin/vendor")
-.constant("getAssignedCompanyUrl", "http://fancymonk.com:9125/api/admin/corporate-companies?type=assigned");
+.constant("getAssignedCompanyUrl", "http://fancymonk.com:9125/api/admin/corporate-companies?type=assigned")
+.constant("getCorporateReviewsUrl", "http://fancymonk.com:9125/api/admin/corporate-reviews?companyId=");
